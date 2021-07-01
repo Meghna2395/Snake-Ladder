4c72d869ef71abd560a4641833d873087dbbc154
Meghna2395 <meghnaborkar23@gmail.com>
2021-07-01 21:28:05 +0530

    SnakeLadderUC2

---
+++ SnakeLadder.java
@@ -0,0 +1,8 @@
+public class SnakeLadder{
+public static void main(String args[]){
+       System.out.println("Welcome to the Snake Ladder");
+      int a = 0;
+      int b = (int) Math.floor(Math.random()*10)%6; 
+		System.out.println(b);
+}
+}
