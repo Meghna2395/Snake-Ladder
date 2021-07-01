f7157861277d617e1a9008194d96990d35ae3f75
Meghna2395 <meghnaborkar23@gmail.com>
2021-07-01 22:14:10 +0530

    SnakeLadderUC3

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,30 @@ public class SnakeLadder{
 public static void main(String args[]){
        System.out.println("Welcome to the Snake Ladder");
       int a = 0;
-      int b = (int) Math.floor(Math.random()*10)%6; 
-		System.out.println(b);
-}
+      int c = 0;
+      int b;
+		
+		int run =(int) Math.floor(Math.random()*10)%3;
+		switch(run){
+		case 1:
+                	b = (int) Math.floor(Math.random()*10)%6;
+			a += b;
+			b++;
+			break;
+		case 2:
+                	b = (int) Math.floor(Math.random()*10)%6;
+			if (a >= b){
+			a -= b;
+			}else {
+			a = 0;
+			}
+			c++;
+			break;
+		default:
+			System.out.println("playing not allowed ");
+	        	c++;
+		}
+		System.out.println(a);
+                System.out.println(c);
+	}
 }
